1c7690facace5681e07a5e222d7d7eb021f50706
Didi <ofisser86@gmail.com>
2020-08-17 16:34:09 +0300

    render html pages

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 # gunicorn
 
-def render_template(template_name = 'index.html'):
-    return "<h1>Hello web without framework</h1>"
+def render_template(template_name = 'index.html', path='/'):
+    html_str = ''
+    with open(template_name, 'r') as f:
+        html_str = f.read()
+    return html_str
 
 def app(environ, start_response):
-    for k, v in environ.items():
-        print(k, v)
+    path = environ.get("PATH_INFO")
+    if path == "/":
+        data = render_template(template_name = 'index.html', path=path)
+    else:
+        data = render_template(template_name = '404.html', path=path)
 
-    data = render_template()
+    
     data = data.encode("UTF-8")
     start_response(
         f"200 OK", [
